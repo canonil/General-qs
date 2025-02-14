6126fe71c3cc46464d6cb1b257c159cba8eb695a
canonil <161255119+canonil@users.noreply.github.com>
2025-02-14 05:35:38 +0100

    Add files via upload

---
+++ pain.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert('Yay!');
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    alert('Lazy fatass');
+});
